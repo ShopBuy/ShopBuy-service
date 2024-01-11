afc0b9366354ce7c98ffde7cfc32c52033e36fcc
nguyentien87xd <nguyentien87xd@gmail.com>
2024-01-11 16:14:41 +0700

    UPDATE BE
    1. Update Profile  (no validate)

--- src/main/java/com/codegym/shopbuyservice/controller/UserController.java
+++ src/main/java/com/codegym/shopbuyservice/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.codegym.shopbuyservice.controller;
 import com.codegym.shopbuyservice.dto.payload.CommonResponse;
+import com.codegym.shopbuyservice.dto.payload.request.UserDetailRequest;
 import com.codegym.shopbuyservice.dto.payload.response.UserDetailResponse;
 
 import com.codegym.shopbuyservice.service.IUserService;
@@ -7,10 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
@@ -37,4 +35,28 @@ public class  UserController {
          return new ResponseEntity<>(commonResponse, HttpStatus.NOT_FOUND);
       }
    }
+
+   @PutMapping("/update-profile")
+   public ResponseEntity<?> updateByEmail(@RequestBody UserDetailRequest userDetailRequest) {
+      try {
+         String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+         UserDetailResponse updatedUser = userService.updateUserByEmail(username, userDetailRequest);
+
+         CommonResponse commonResponse = new CommonResponse();
+         commonResponse.setData(updatedUser);
+         commonResponse.setMessage("Update profile for user success !");
+         commonResponse.setStatusCode(HttpStatus.OK);
+
+         return new ResponseEntity<>(commonResponse, HttpStatus.OK);
+      } catch (Exception e) {
+         CommonResponse commonResponse = new CommonResponse();
+         commonResponse.setData(null);
+         commonResponse.setMessage("Update profile for user fail");
+         commonResponse.setStatusCode(HttpStatus.NOT_FOUND);
+
+         return new ResponseEntity<>(commonResponse, HttpStatus.NOT_FOUND);
+      }
+   }
+
 }
